7b2a405ef06496afc122d32a17b93096dca830e0
satyam-1906 <scientificsatyam1906@gmail.com>
2026-03-30 22:04:02 +0530

    converted it to a function

--- fault_detection/imageAnalysis.py
+++ fault_detection/imageAnalysis.py
@@ -1,35 +1,34 @@
-import cv2
-import requests
-import supervision as sv
-from inference_sdk import InferenceHTTPClient
-from PIL import Image
+def detect_faults(image_path):
+  import cv2
+  import requests
+  import supervision as sv
+  from inference_sdk import InferenceHTTPClient
+  from PIL import Image
 
-file_path = '/content/United_States_003627.jpg'
+  img = Image.open(image_path)
 
-img = Image.open(file_path)
+  img.show()
 
-img.show()
+  with open('api_key.txt', 'r') as f:
+    api_key = f.read()
 
-with open('api_key.txt', 'r') as f:
-  api_key = f.read()
-
-client = InferenceHTTPClient(
+  client = InferenceHTTPClient(
     api_url="https://serverless.roboflow.com",
     api_key=api_key,
-)
-results = client.infer(img, model_id="construction-defects/3")
+  )
+  results = client.infer(img, model_id="construction-defects/3")
 
-detections = sv.Detections.from_inference(results)
+  detections = sv.Detections.from_inference(results)
 
-labels = [f'{i['class']} {i['confidence']:.3f}' for i in results['predictions']]
+  labels = [f'{i['class']} {i['confidence']:.3f}' for i in results['predictions']]
 
-if detections.is_empty():
+  if detections.is_empty():
     print("No detections found")
     sv.plot_image(img)
-else:
-  annotated_image = sv.BoxAnnotator().annotate(scene=img, detections=detections)
-  label_annotator = sv.LabelAnnotator()
-  annotated_frame = label_annotator.annotate(
+  else:
+    annotated_image = sv.BoxAnnotator().annotate(scene=img, detections=detections)
+    label_annotator = sv.LabelAnnotator()
+    annotated_frame = label_annotator.annotate(
       scene=img.copy(),
       detections=detections,
       labels=labels
